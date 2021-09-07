0296b6f8a5f4da2d95fc1ce439e5b1021e7994bb
goldencoin-foundation <87344862+goldencoin-foundation@users.noreply.github.com>
2021-09-06 17:25:22 -0700

    Update main.h

--- src/main.h
+++ src/main.h
@@ -19,22 +19,22 @@ class CBlockIndex;
 class CKeyItem;
 class CReserveKey;
 
-class CAddress;
+class CAddress
 class CInv;
 class CNode;
 
 struct CBlockIndexWorkComparator;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
-static const unsigned int MAX_BLOCK_SIZE = 16000000;                      // 64MB block hard limit
+static const unsigned int MAX_BLOCK_SIZE = 128000000;                      // 64MB block hard limit
 /** Obsolete: maximum size for mined blocks */
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/4;         // 16MB  block soft limit
 /** Default for -blockmaxsize, maximum size for mined blocks **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 4000000;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 32000000;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
-static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 17000;
+static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 170000;
 /** The maximum size for transactions we're willing to relay/mine */
-static const unsigned int MAX_STANDARD_TX_SIZE = 10000;
+static const unsigned int MAX_STANDARD_TX_SIZE = 250000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 /** Default for -maxorphantx, maximum number of orphan transactions kept in memory */
